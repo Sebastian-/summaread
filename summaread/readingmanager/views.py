87a73a4e3c861e7740ba885da861478bcdf8ff79
Sebastian- <hmurgu@hotmail.com>
2020-03-23 14:43:12 -0700

    add basic list view for books

--- summaread/readingmanager/views.py
+++ summaread/readingmanager/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(LoginRequiredMixin, ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'readingmanager/book_list.html'
+
+    def get_queryset(self):
+        queryset = Book.objects.filter(user=self.request.user)
+        return queryset
