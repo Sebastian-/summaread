df4074aebe9977c6124be8b202084e1d0d638d8f
Sebastian- <hmurgu@hotmail.com>
2020-03-24 09:52:26 -0700

    create summary model and migration

--- summaread/readingmanager/models.py
+++ summaread/readingmanager/models.py
@@ -12,3 +12,15 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Summary(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='book',
+    )
+    date = models.DateTimeField(auto_now=True)
+    startPage = models.IntegerField(name='Start Page')
+    endPage = models.IntegerField(name='End Page')
+    summary = models.TextField()
