6c9b39a2a13e8ccd16a2c3265f574c64743b58f2
Sebastian- <hmurgu@hotmail.com>
2020-03-24 10:14:41 -0700

    add summaries to admin panel

--- summaread/readingmanager/admin.py
+++ summaread/readingmanager/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Summary
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "user",)
 
 
+class SummaryAdmin(admin.ModelAdmin):
+    list_display = ("book", "title", "Start Page", "End Page")
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Summary, SummaryAdmin)
